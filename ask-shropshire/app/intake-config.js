6262a9251582c5b9214fe95d8664a108b997ec81
chipblock2 <66038232+chipblock2@users.noreply.github.com>
2026-09-11 09:23:40 -0700

    Connect secure business update intake endpoint

--- ask-shropshire/app/intake-config.js
+++ ask-shropshire/app/intake-config.js
@@ -3,5 +3,5 @@ window.ASK_SHROPSHIRE_INTAKE_ENDPOINTS={
   business_claim:'https://form.jotform.com/262533549106053',
   alerts:'https://form.jotform.com/262533678449066',
   offer:'https://form.jotform.com/262534146466056',
-  business_update:null
+  business_update:'https://form.jotform.com/262533808049057'
 };
