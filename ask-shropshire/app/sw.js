c96e4c12154dcff004dc52e500400557040906da
chipblock2 <66038232+chipblock2@users.noreply.github.com>
2026-09-11 06:22:08 -0700

    Cache verified business update flow in Ask Shropshire PWA

--- ask-shropshire/app/sw.js
+++ ask-shropshire/app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='ask-shropshire-v6';
-const APP_SHELL=['./','./index.html','./app.css','./app.js','./pwa.js','./feed-patch.js','./manifest.webmanifest','./privacy.html','./accessibility.html','./submit-event.html','./claim-business.html','./alerts.html','./deals.html','./submit-offer.html','./intake.js','./deals.js','./icon.svg','./maskable.svg','./icon-192.png','./icon-512.png'];
+const CACHE='ask-shropshire-v7';
+const APP_SHELL=['./','./index.html','./app.css','./app.js','./pwa.js','./feed-patch.js','./manifest.webmanifest','./privacy.html','./accessibility.html','./submit-event.html','./claim-business.html','./update-business.html','./alerts.html','./deals.html','./submit-offer.html','./intake.js','./deals.js','./icon.svg','./maskable.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
